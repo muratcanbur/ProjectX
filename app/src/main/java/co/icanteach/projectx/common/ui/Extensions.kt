a0ec4f7b525038c4b85c2cf28ca566954061b1fd
muratcanbur <muratcan.bur@trendyol.com>
2019-06-15 09:14:25 +0300

    update on extensions

--- app/src/main/java/co/icanteach/projectx/common/ui/Extensions.kt
+++ app/src/main/java/co/icanteach/projectx/common/ui/Extensions.kt
@@ -5,7 +5,22 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import co.icanteach.projectx.common.Resource
+import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
 
+
+/**
+ * http://kotlinextensions.com/
+ */
 fun <T : ViewDataBinding> ViewGroup?.inflate(@LayoutRes layoutId: Int, attachToParent: Boolean = true): T {
     return DataBindingUtil.inflate(LayoutInflater.from(this!!.context), layoutId, this, attachToParent)
+}
+
+
+/**
+ * https://blog.danlew.net/2015/03/02/dont-break-the-chain/
+ */
+fun <T> applyLoading(): ObservableTransformer<Resource<T>, Resource<T>> = ObservableTransformer { upstream ->
+    Observable.just(Resource.loading<T>()).concatWith(upstream)
 }
--- app/src/main/java/co/icanteach/projectx/data/feed/MoviesRepository.kt
+++ app/src/main/java/co/icanteach/projectx/data/feed/MoviesRepository.kt
@@ -1,17 +1,11 @@
 package co.icanteach.projectx.data.feed
 
 import co.icanteach.projectx.common.Resource
+import co.icanteach.projectx.common.ui.applyLoading
 import io.reactivex.Observable
-import io.reactivex.ObservableTransformer
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-/**
- * https://blog.danlew.net/2015/03/02/dont-break-the-chain/
- */
-fun <T> applyLoading(): ObservableTransformer<Resource<T>, Resource<T>> = ObservableTransformer { upstream ->
-    Observable.just(Resource.loading<T>()).concatWith(upstream)
-}
 
 class MoviesRepository @Inject constructor(private val moviesRemoteDataSource: MoviesRemoteDataSource) {
 
@@ -19,7 +13,7 @@ class MoviesRepository @Inject constructor(private val moviesRemoteDataSource: M
         moviesRemoteDataSource
             .fetchMovies(page)
             .map { Resource.success(it) }
-            .onErrorReturn { Resource.error<PopularTVShowsResponse>(it) }
+            .onErrorReturn { Resource.error(it) }
             .subscribeOn(Schedulers.io())
             .compose(applyLoading())
 }
