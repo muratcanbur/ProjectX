cafbc4e04bb02fcd87e4e33e4b7eaae1a4558460
muratcanbur <muratcan.bur@trendyol.com>
2019-06-15 12:48:04 +0300

    update getTvShowRating

--- app/src/main/java/co/icanteach/projectx/ui/PopularTVShowsFeedItemViewState.kt
+++ app/src/main/java/co/icanteach/projectx/ui/PopularTVShowsFeedItemViewState.kt
@@ -1,5 +1,7 @@
 package co.icanteach.projectx.ui
 
+import android.content.Context
+import co.icanteach.projectx.R
 import co.icanteach.projectx.data.feed.TvShow
 
 class PopularTVShowsFeedItemViewState(private val tvShow: TvShow) {
@@ -8,5 +10,8 @@ class PopularTVShowsFeedItemViewState(private val tvShow: TvShow) {
 
     fun getTvShowName() = tvShow.name
 
-    fun getTvShowRating() = tvShow.rating
+    fun getTvShowRating(context: Context): String {
+        val placeHolder = context.getString(R.string.movie_rating)
+        return String.format(placeHolder, tvShow.rating)
+    }
 }
