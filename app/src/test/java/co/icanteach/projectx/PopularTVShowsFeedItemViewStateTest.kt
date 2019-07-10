d95b6230a04b2b54187325879704ae772484031a
muratcanbur <muratcan.bur@trendyol.com>
2019-07-10 20:44:12 +0300

    remove wrong test implementation

--- app/src/test/java/co/icanteach/projectx/PopularTVShowsFeedItemViewStateTest.kt
+++ app/src/test/java/co/icanteach/projectx/PopularTVShowsFeedItemViewStateTest.kt
@@ -35,20 +35,6 @@ class PopularTVShowsFeedItemViewStateTest {
         Truth.assertThat(actualResult).isEqualTo("/hlLXt2tOPT6RRnjiUmoxyG1LTFi.jpg")
     }
 
-    @Test
-    fun `should match rating count for given tv show`() {
-
-        // Given
-        val tvShow = createDummyTvShow()
-        val givenViewState = PopularTVShowsFeedItemViewState(tvShow)
-
-        // When
-        val actualResult = givenViewState.getTvShowRating()
-
-        // Then
-        Truth.assertThat(actualResult).isEqualTo("8.3")
-    }
-
     private fun createDummyTvShow(): TvShow {
         return TvShow(name = "Chernobyl", imageUrl = "/hlLXt2tOPT6RRnjiUmoxyG1LTFi.jpg", rating = "8.3")
     }
